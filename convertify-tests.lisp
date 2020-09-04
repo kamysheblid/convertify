71feb527ba0d3bf33a7e4eb0473b16b4515d23a5
kamysheblid <kamysheblid@gmail.com>
2020-09-04 14:21:31 -0400

    fixed the testing framework from lisp-unit to the better clunit2

--- convertify-tests.lisp
+++ convertify-tests.lisp
@@ -9,7 +9,9 @@
 (defvar char-letter-list (list #\A #\B #\C))
 (defvar letter-list (list 'A 'B 'C))
 (defvar number-list (list 1 2 3))
+(defvar num-list (list 123))
 (defvar char-number-list (list #\1 #\2 #\3))
+(defvar char-list (list #\A))
 
 (defvar char-letter-array #(#\A #\B #\C))
 (defvar char-symbol-array #(A B C))
@@ -24,70 +26,35 @@
 (defvar number-symbol '123)
 
 (defvar num 123)
-
+(defvar chr #\A)
 
 ;;; test arrayify
 
 (defsuite array-suite ())
 
 (deftest number-array->array (array-suite) (assert-equalp number-array (arrayify number-array)))
-
 (deftest letter-array->array (array-suite) (assert-equalp letter-array (arrayify letter-array)))
-
 (deftest char-letter-array->array (array-suite) (assert-equalp char-letter-array (arrayify char-letter-array)))
-
 (deftest char-letter-list->array (array-suite) (assert-equalp char-letter-array (arrayify char-letter-list)))
-
 (deftest number-list->array (array-suite) (assert-equalp number-array (arrayify number-list)))
-
 (deftest letter-list->array (array-suite) (assert-equalp char-symbol-array (arrayify letter-list)))
-
 (deftest char-letter-list->array (array-suite) (assert-equalp char-letter-array (arrayify char-letter-list)))
-
 (deftest number-string->array (array-suite) (assert-equalp char-number-array (arrayify number-string)))
-
 (deftest letter-string->array (array-suite) (assert-equalp char-letter-array (arrayify letter-string)))
-
 (deftest num->array (array-suite) (assert-equalp number-array (arrayify num)))
-
 (deftest number-symbol->array (array-suite) (assert-equalp number-array (arrayify number-symbol)))
-
 (deftest letter-symbol->array (array-suite) (assert-equalp char-letter-array (arrayify letter-symbol)))
-
-;; (define-test arrayify-test
-;;     (assert-equal number-array (arrayify number-array))
-;;   (assert-equal letter-array (arrayify letter-array))
-;;   (assert-equal char-letter-array (arrayify char-letter-array))
-;;   (assert-equal char-letter-array (arrayify char-letter-list))
-;;   (assert-equal number-array (arrayify number-list))
-
-;;   (assert-equal char-letter-array (arrayify letter-list))
-;;   (assert-equal char-letter-array (arrayify char-letter-list))
-  
-;;   (assert-equal char-number-array (arrayify number-string))
-;;   (assert-equal char-letter-array (arrayify letter-string))
-
-;;   (assert-equal number-array (arrayify num))
-
-;;   (assert-equal number-array (arrayify number-symbol))
-;;   (assert-equal char-letter-array (arrayify letter-symbol)))
 
 ;;; test listify
 
-;; (define-test listify-test
-;;     (assert-equal (listify char-letter-list) char-letter-list)
-;;   (assert-equal (listify letter-list) letter-list)
-;;   (assert-equal (listify number-list) number-list)
+(defsuite list-suite ())
 
-;;   (assert-equal (listify #\a) (list #\a))
-;;   (assert-equal (listify 1) (list 1))
-  
-;;   (assert-equal (listify letter-string) char-letter-list)
-;;   (assert-equal (listify number-string) char-number-list)
-  
-;;   (assert-equal (listify number-string) number-list)
-;;   (assert-equal (listify letter-string) char-letter-list)
-
-;;   (assert-equal (listify letter-array) letter-list)
-
-;;   (assert-equal (listify 123) number-list))
+(deftest  char-letter-list->list (list-suite) (assert-equalp (listify char-letter-list) char-letter-list))
+(deftest  letter-list->list (list-suite) (assert-equalp (listify letter-list) letter-list))
+(deftest number-list->list (list-suite) (assert-equalp (listify number-list) number-list))
+(deftest char->list (list-suite) (assert-equalp (listify chr) char-list))
+(deftest num->list (list-suite) (assert-equalp (listify num) number-list))
+(deftest letter-string->list (list-suite) (assert-equalp (listify letter-string) char-letter-list))
+(deftest number-string->list (list-suite) (assert-equalp (listify number-string) char-number-list))
+(deftest letter-string->list (list-suite) (assert-equalp (listify letter-string) char-letter-list))
+(deftest letter-array->list (list-suite) (assert-equalp (listify letter-array) letter-list))
