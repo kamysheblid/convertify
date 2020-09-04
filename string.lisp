a4b7724c67daf7e83ac7befef8496791b62534e9
kamysheblid <kamysheblid@gmail.com>
2020-09-04 12:57:40 -0400

    renaming funcs for easier use, adding ify at end of functions

--- string.lisp
+++ string.lisp
@@ -1,15 +1,31 @@
-;;;; conversions.lisp
+;;;; string.lisp
 
-(in-package #:conversions)
+(in-package #:convertify)
 
-(defgeneric string (arg))
+(defgeneric stringify (arg))
 
-(defmethod string ((
-(defun array-to-string (arr)
+(defmethod stringify ((str string))
+  str)
+
+(defmethod stringify ((char character))
+  (format nil "~A" char))
+
+(defmethod stringify ((lst list))
+  (concatenate 'string (mapcar #'characterify lst)))
+
+(defmethod stringify ((num number))
+  (format nil "~A" num))
+
+(defmethod stringify ((sym symbol))
+  (format nil "~A" sym))
+
+(defmethod stringify ((arr array))
+  nil)
+
+(defun array-to-stringify (arr)
   (declare (array arr))
   (let ((lst (coerce arr 'list)))
     (apply #'concatenate
 	   (mapcar
 	    (lambda (num)
 	      (format nil "~A" num))))))
-
