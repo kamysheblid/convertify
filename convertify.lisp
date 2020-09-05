c8facd62e5fc698ddbaee24709dd7654e64073a7
kamysheblid <kamysheblid@gmail.com>
2020-09-05 19:05:19 -0400

    adding more instructions to main function, convertify

--- convertify.lisp
+++ convertify.lisp
@@ -3,7 +3,10 @@
 (in-package #:convertify)
 
 (defun convertify (arg type)
-  "Convert ARG to TYPE."
+  "Convert ARG to TYPE.
+TYPE can be the symbol, string, character, number, array, or list.
+The type of ARG can be symbol, string, character, number, array, or list.
+"
   (case type
     (string (stringify arg))
     (number (numberify arg))
